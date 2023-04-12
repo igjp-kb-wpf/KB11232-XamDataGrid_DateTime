df6cf8a94d2cce025bada4048c81a20688496c7f
mkamiishi <mkamiishi@infragistics.com>
2023-04-12 10:48:27 +0900

    Initial commit
    初回コミット

---
+++ XamDataGrid_DateTime/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XamDataGrid_DateTime
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<TestData> testData = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            testData = new ObservableCollection<TestData>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                testData.Add(new TestData { Id = i, Test1 = DateTime.Today.AddMonths(i), Test2 = "Test" + i });
+            }
+
+            this.DataContext = testData;
+        }
+    }
+
+    public class TestData : INotifyPropertyChanged
+    {
+        private int m_id;
+        public int Id
+        {
+            get { return m_id; }
+            set
+            {
+                m_id = value;
+                NotifyPropertyChanged("Id");
+            }
+        }
+
+        private DateTime m_test1;
+        public DateTime Test1
+        {
+            get { return m_test1; }
+            set
+            {
+                m_test1 = value;
+                NotifyPropertyChanged("Test1");
+            }
+        }
+
+        private String m_test2;
+        public String Test2
+        {
+            get { return m_test2; }
+            set
+            {
+                m_test2 = value;
+                NotifyPropertyChanged("Test2");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
